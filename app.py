2a04d8122e9bd281e3b30ece65e0fec1df950dfa
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-10-12 18:56:54 -0300

    feat: adding cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import pickle
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/send-form',methods=['POST'])
 def createTask():
